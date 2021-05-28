3767fe7e1bfc7b55cb589f22b6226ddf202da600
andykjb <84149105+andykjb@users.noreply.github.com>
2021-05-28 13:22:39 +0200

    initial commit

---
+++ resources/JavaScript/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', (event) => {
+    return document.getElementById('front-page-logo').style.animation = "null";
+});
+
+/*
+const ready = function() {
+    document.getElementById('front-page-logo').style.animation = "null";
+};
+
+document.addEventListener('DOMcontentloaded', ready)
+*/
